c3d0146c7a2ad9a1653ae98aa10c953f5d571e8f
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-08 21:30:56 -0500

    Paso 6: eje4 = vmejorada

--- eje4.js
+++ eje4.js
@@ -1,11 +1,17 @@
 // Paso 6: 
 
-let numero = Number(prompt("Ingrese un número:"));
-let cuadrado = Math.pow(numero, 2);
-let cubo = Math.pow(numero, 3);
-let raizCuadrada = Math.sqrt(numero);
+function cuadrado(num) {
+    return Math.pow(num, 2);
+}
+function cubo(num) {
+    return Math.pow(num, 3);
+}
+function raizCuadrada(num) {
+    return Math.sqrt(num);
+}
 
-console.log("Número ingresado:", numero);
-console.log("Cuadrado:", cuadrado);
-console.log("Cubo:", cubo);
-console.log("Raíz cuadrada:", raizCuadrada);
+let numero = Number(prompt("Ingrese un número:"));
+console.log("Número:", numero);
+console.log("Cuadrado:", cuadrado(numero));
+console.log("Cubo:", cubo(numero));
+console.log("Raíz cuadrada:", raizCuadrada(numero));
