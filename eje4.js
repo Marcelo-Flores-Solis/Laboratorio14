555d5b88e83092cb10ed4c0356d9a5b47ee7ab26
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-08 21:07:58 -0500

    Paso 6: eje4 = Numero y operaciones

---
+++ eje4.js
@@ -0,0 +1,11 @@
+// Paso 6: 
+
+let numero = Number(prompt("Ingrese un número:"));
+let cuadrado = Math.pow(numero, 2);
+let cubo = Math.pow(numero, 3);
+let raizCuadrada = Math.sqrt(numero);
+
+console.log("Número ingresado:", numero);
+console.log("Cuadrado:", cuadrado);
+console.log("Cubo:", cubo);
+console.log("Raíz cuadrada:", raizCuadrada);
