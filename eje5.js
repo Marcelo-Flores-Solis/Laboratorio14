017c2d12c19aa0ff85734a4ee8f316aeb7dfdfc1
Marcelo-Flores-Solis <mfloresso@unsa.edu.pe>
2025-11-08 21:34:11 -0500

    Paso 7: eje5 = vmejorada

--- eje5.js
+++ eje5.js
@@ -1,19 +1,29 @@
 // Paso 7: 
 
-function gradosARadianes(grados) {
-    let radianes = grados * (Math.PI / 180);
-    console.log(grados + " grados = " + radianes + " radianes");
-    console.log("Seno:", Math.sin(radianes));
-    console.log("Coseno:", Math.cos(radianes));
-}
 
+function gradosARadianes(grados) {
+    return grados * (Math.PI / 180);
+}
 function radianesAGrados(radianes) {
-    let grados = radianes * (180 / Math.PI);
-    console.log(radianes + " radianes = " + grados + " grados");
+    return radianes * (180 / Math.PI);
+}
+function seno(radianes) {
+    return Math.sin(radianes);
+}
+function coseno(radianes) {
+    return Math.cos(radianes);
 }
 
 let grados = Number(prompt("Ingrese los grados a convertir:"));
-gradosARadianes(grados);
+console.log("Grados usados:", grados);
 
-let radianes = Number(prompt("Ingrese los radianes a convertir:"));
-radianesAGrados(radianes);
+let radianes = gradosARadianes(grados);
+console.log("Radianes:", radianes);
+console.log("Seno:", seno(radianes));
+console.log("Coseno:", coseno(radianes));
+
+let radianesInput = Number(prompt("Ingrese los radianes a convertir:"));
+console.log("Radianes usados:", radianesInput);
+
+let gradosConvertidos = radianesAGrados(radianesInput);
+console.log("Grados:", gradosConvertidos);
